bddd15458f368597996f77752bd71394f2c091f9
chirag tekwani <ctekwani051@gmail.com>
2024-10-16 15:08:46 +0530

    Add files via upload

---
+++ stack_using_stl.cpp
@@ -0,0 +1,17 @@
+// Name : Chirag Tekwani
+// PRN : 23070123042
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    stack<int>st;
+st.push(30);
+st.push(20);
+st.push(10);
+
+cout<< "Top element of the stack: " <<st.top() <<endl;
+
+    return 0;
+}
